ae6b4d33f88c4e7436d85bdd80b43a6dc461deff
Renée Cruz <reneebasscruz@gmail.com>
2019-11-26 12:25:50 -0500

    user account component renders user data from redux using hooks

--- src/Pages/Account.js
+++ src/Pages/Account.js
@@ -9,10 +9,11 @@ const Account = () => {
         <div>
             Item: {item}
             <input type="text" value={item} onChange={(e) => setItem(e.target.value)}/>
-           <h1>Welcome, {user.username}!</h1> 
-           <h2>Name: {user.firstName} {user.lastName}</h2> 
+           <h1>Welcome, {user.first_name}!</h1> 
+           <h2>Name: {user.first_name} {user.last_name}</h2> 
            <h2>Email: {user.email}</h2> 
            <h2>Username: {user.username}</h2> 
+           <img src={user.img_url} alt={user.first_name}/>
         </div>
     )
 }
--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -6,7 +6,11 @@ class Signup extends Component {
 
     state = {
     username: '',
-    password: ''
+    password: '',
+    email: '',
+    img_url: '',
+    first_name: '',
+    last_name: ''
     };
 
     handleChange = e => this.setState({ [e.target.name]: e.target.value });
@@ -17,19 +21,59 @@ class Signup extends Component {
     };
 
         render() {
-            const { username, password } = this.state;
+            const { username, password, email, img_url, first_name, last_name } = this.state;
             return (
               <>
                 
                 <form onSubmit={this.handleSubmit}>
                 <h1>Signup for an Account</h1>
 
-                <label>username</label>
+                <label>First Name</label>
+                  <input
+                    type="text"
+                    name="first_name"
+                    placeholder="first name"
+                    value={first_name}
+                    onChange={this.handleChange}
+                  />
+                  <br/>
+
+                  <label>Last Name</label>
+                  <input
+                    type="text"
+                    name="last_name"
+                    placeholder="last name"
+                    value={last_name}
+                    onChange={this.handleChange}
+                  />
+                  <br/>
+
+                  <label>Email</label>
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="email"
+                    value={email}
+                    onChange={this.handleChange}
+                  />
+                  <br/>
+
+                  <label>username</label>
                   <input
                     type="text"
                     name="username"
                     placeholder="username"
                     value={username}
+                    onChange={this.handleChange}
+                  />
+                  <br/>
+
+                <label>Image</label>
+                  <input
+                    type="text"
+                    name="img_url"
+                    placeholder="image url"
+                    value={img_url}
                     onChange={this.handleChange}
                   />
                   <br/>
